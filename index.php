dfa150be2fde014e136d8cceb36a7f43b297bc6c
Patty <patty2.4@hotmail.com>
2018-12-11 15:54:01 -0500

    Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+<table width="100%" border="0">
+  <tr>
+    <td height="55" colspan="4"><table width="588" border="0" align="center">
+      <tr>
+        <td width="25%" background="imgs/fondomenuinferior.png" align="center"><a href="clientes.php" target="cuerpoby"><div style="width: 100%; height: 50px; margin-top: 20px"  ><font color="#FFFFFF"><b>CLIENTES</b></font></div></a></td>
+        <td width="23%" background="imgs/fondomenuinferior.png" align="center"><a href="productos.php" target="cuerpoby"><div style="width: 100%; height: 50px; margin-top: 20px" ><font color="#FFFFFF"><b>PRODUCTOS</b></font></div></a></td>
+   <td width="25%" background="imgs/fondomenuinferior.png" align="center"><a href="facturas.php" target="cuerpoby"><div style="width: 100%; height: 50px; margin-top: 20px" ><font color="#FFFFFF"><b>FACTURACION</b></font></div></a></td>
+        <td width="27%" background="imgs/fondomenuinferior.png" align="center"><a href="clientes.php" target="cuerpoby"><div style="width: 100%; height: 50px; margin-top: 20px" ><font color="#FFFFFF"><b>MIS VENTAS</b></font></div></a></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td colspan="4"><iframe frameborder="0" id="cuerpoby"  name="cuerpoby" src="productos.php" width="100%" height="500"></iframe></td>
+  </tr>
+</table>
+</body>
+</html>
---
+++ model/conexion.php
@@ -0,0 +1,3 @@
+<?php
+$conn = pg_connect("host=ec2-54-204-36-249.compute-1.amazonaws.com port=5432 dbname=d22eokasrcc2a4 user=fwrgdmkgnuxwuv password=d574cc1b39cd919416de09c501765403fc3481805e8b41b83edd8dd6cf12e236");
+?>
